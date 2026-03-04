7fe5f6f98032034a5941272ec33d35c82f4c2bd4
Namit Magotra <namit130506@gmail.com>
2026-03-04 21:45:18 +0530

    Reworked on the calculator code

--- Mini-Projects/Calculator/Calculator.py
+++ Mini-Projects/Calculator/Calculator.py
@@ -1,13 +1,39 @@
-operator=input("Enter your input (+ - * /) :")
-num1=float(input("Enter your number! :"))
-num2=float(input("Enter your number! :"))
-if operator=="+" :
-    print(num1+num2)
-elif operator=="-" :
-    print(num1-num2)
-elif operator =="*" :
-    print(num1*num2)
-elif operator =="/" :
-    print (num1/num2)
-else :
-    print ("Operator invalid please retry!")
+import art
+def add(n1, n2):
+    return n1 + n2
+
+
+def sub(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {"+": add, "-": sub, "*": multiply, "/": divide}
+def calculator() :
+    print(art.logo)
+    should_accumulate=True
+    a = float(input("Enter first number :"))
+    while should_accumulate:
+        for operators in operations:
+            print(operators)
+        operator = input("Choose the operation you want to perform (+,-,*,/) :")
+        b = float(input("Enter second number :"))
+        answer = operations[operator](a, b)
+        print(f"{a} {operator} {b} = {answer}")
+
+        choice=input(f"Type 'y' if you want to continue with {answer}, or type 'no' to start new calculation :")
+        
+        if choice=='y' :
+            a=answer
+        else :
+            should_accumulate=False
+            print("\n"*5)
+            calculator() #recursion
+calculator()
