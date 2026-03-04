3d074d87f9275a51e59e92ab0e44fb029e51a622
Namit Magotra <namit130506@gmail.com>
2026-03-04 21:43:12 +0530

    Reworked on the Rock Paper Scissors Game code

--- Mega-Projects/Rock-Paper-Scissors/Rock-Paper-Scissors.py
+++ Mega-Projects/Rock-Paper-Scissors/Rock-Paper-Scissors.py
@@ -1,36 +1,58 @@
 import random
+rock=("""
+      Rock!
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+""")
 
-options=("rock","paper","scissors")
-running=True
+paper=("""
+       Paper!
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+""")
 
-while running :
+scissors=("""
+          Scissor
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+""")
 
-    player=None
-    computer=random.choice(options)
 
-    while player not in options :
-        player=input("Enter a choice(Rock,Paper,Scissors) :")
+choice=[rock,paper,scissors]
+user_choice=int(input("What do you want tc choose?(Type 0 for rock , 1 for paper , 2 for scissors) :"))
+if user_choice>=0 and user_choice<=2 :
+    print("You chose :")
+    print(choice[user_choice])
 
-    print(f"Player :{player}")
-    print(f"Computer :{computer}")
+computer_choice=random.randint(0,2)
+print("Computer chose :")
+print(choice[computer_choice])
 
-    if player==computer :
-        print ("its a tie!")
+if user_choice>2 or user_choice<0 :
+    print("Invalid Number chosen , You lost!")
+elif user_choice==0 and computer_choice==2 :
+    print("You win!")
 
-    elif player == "rock" and computer== "scissors":
-        print("You win!")
+elif computer_choice==0 and user_choice==2 :
+    print("You lose!")
 
-    elif player == "paper" and computer== "rock":
-        print("You win!")
+elif user_choice>computer_choice :
+    print("You win!")
 
-    elif player == "scissors" and computer== "paper":
-        print("You win!")
+elif user_choice<computer_choice:
+    print("You lose!")
 
-    else :
-        print("You lose !")
-
-    play_again =input("Play again? (Y/N) :").lower()
-    if not play_again=="y" :
-        running=False
-     
-print("Thanks for playing!")
+elif user_choice==computer_choice :
+    print("Its a Draw!")
